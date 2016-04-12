fdba9b3b87ab8b2f96a3c91c9aa2b24021123dbf
Josef Kolář <thejoeejoee@gmail.com>
2016-04-12 23:43:21 +0200

    added fonts, fixed colors inverting

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import time
 from copy import copy
 from datetime import timedelta
-from tkinter import Frame, Tk, Button, Label, Widget
+from tkinter import Frame, Tk, Button, Label
 from tkinter.constants import BOTH, W, N, E, S
-
-COPYRIGHT_FONT = ('Helvetica', 28)
-LABELS_FONT = ('Helvetica', 50)
-SECTION_FONT = ('Helvetica', 76)
-TIME_FONT = ('Helvetica', 84)
+from tkinter.font import Font, BOLD
 
 EXAM_SECTIONS = (
     ('Introduction', timedelta(minutes=.5)),
@@ -19,6 +15,8 @@ EXAM_SECTIONS = (
     ('Part IV.', timedelta(minutes=3)),
     ('The End', timedelta(minutes=3)),
 )
+LIGHT = 'gainsboro'
+DARK = 'black'
 
 format_delta = lambda delta: '{}:{:02}'.format(delta.seconds // 60, delta.seconds % 60)
 
@@ -28,6 +26,7 @@ class TimerFrame(Frame):
     start_btn = current_time_lbl = copyright_lbl = remaining_time_frame = None
     section_title_lbl = elapsed_time_lbl = remaining_time_lbl = None
     inverting_parts = []
+    ui_light = True
 
     # timer logic
     actual_section = -1
@@ -36,34 +35,40 @@ class TimerFrame(Frame):
     def __init__(self, master=None, cnf={}, **kw):
         super().__init__(master, cnf, **kw)
 
+        self.COPYRIGHT_FONT = Font(master, family='Helvetica', size=28)
+        self.LABELS_FONT = Font(master, family='Helvetica', size=50)
+        self.SECTION_FONT = Font(master, family='Helvetica', size=76)
+        self.TIME_FONT = Font(master, family='Helvetica', size=96, weight=BOLD)
+
         self.setup_ui()
         self._update_current_time()
 
     def setup_ui(self):
+        self.configure(bg='gainsboro')
         self.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(1, weight=1)
 
-        self.start_btn = Button(self, command=self.start_timer, text='START!', font=LABELS_FONT)
+        self.start_btn = Button(self, command=self.start_timer, text='START!', font=self.LABELS_FONT)
         self.start_btn.grid(row=2, column=1, sticky=S)
 
-        self.current_time_lbl = Label(self, font=LABELS_FONT)
+        self.current_time_lbl = Label(self, font=self.LABELS_FONT)
         self.current_time_lbl.grid(row=2, column=0, sticky=W + S)
 
-        self.copyright_lbl = Label(self, text='© Josef Kolář, 2016', font=COPYRIGHT_FONT)
+        self.copyright_lbl = Label(self, text='© Josef Kolář, 2016', font=self.COPYRIGHT_FONT)
         self.copyright_lbl.grid(column=2, row=0, sticky=N + E)
 
-        self.section_title_lbl = Label(self, text=EXAM_SECTIONS[0][0], font=SECTION_FONT)
+        self.section_title_lbl = Label(self, text=EXAM_SECTIONS[0][0], font=self.SECTION_FONT)
         self.section_title_lbl.grid(column=1, row=0, sticky=N)
 
-        self.elapsed_time_lbl = Label(self, text='0:00', font=LABELS_FONT)
+        self.elapsed_time_lbl = Label(self, text='0:00', font=self.LABELS_FONT)
         self.elapsed_time_lbl.grid(column=0, row=0, sticky=N + W)
 
         self.remaining_time_frame = Frame(self)
         self.remaining_time_frame.grid(column=1, row=1)
 
-        self.remaining_time_lbl = Label(self.remaining_time_frame, text='0:00', font=TIME_FONT)
+        self.remaining_time_lbl = Label(self.remaining_time_frame, text='0:00', font=self.TIME_FONT)
         self.remaining_time_lbl.pack()
 
         self.inverting_parts.extend((
@@ -72,7 +77,7 @@ class TimerFrame(Frame):
             self.section_title_lbl,
             self.remaining_time_lbl,
             self.start_btn,
-            self.master
+            self.elapsed_time_lbl
         ))
 
     def _update_current_time(self):
@@ -80,9 +85,11 @@ class TimerFrame(Frame):
         self.master.after(1000, self._update_current_time)
 
     def _invert_ui(self):
+        self.ui_light = not self.ui_light
+        self.master.configure(bg=(LIGHT, DARK)[self.ui_light])
+        self.configure(bg=(LIGHT, DARK)[self.ui_light])
         for part in self.inverting_parts:
-            if isinstance(part, Widget):
-                part['background'], part['foreground'] = part['foreground'], part['background']
+            part['background'], part['foreground'] = part['foreground'], part['background']
 
     def start_timer(self):
         self._set_next_section()
@@ -97,7 +104,10 @@ class TimerFrame(Frame):
             self._invert_ui()
 
         self.remaining_time_lbl.configure(text=format_delta(self.section_remaining))
-        self.master.after(1000, self.update_timer)
+        self.elapsed_time_lbl.configure(
+            text=format_delta(EXAM_SECTIONS[self.actual_section][1] - self.section_remaining)
+        )
+        self.master.after(100, self.update_timer)
 
     def _set_next_section(self):
         self.actual_section += 1
@@ -108,6 +118,7 @@ class TimerFrame(Frame):
 
 if __name__ == '__main__':
     master = Tk()
+    master.title = 'ÚMZ ANJ - časovač'
     timer_frame = TimerFrame(master)
 
     timer_frame.pack(fill=BOTH)
