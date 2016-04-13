cfc5278f11b226e418cf37894ab7745f06855827
Josef Kolář <thejoeejoee@gmail.com>
2016-04-13 10:53:42 +0200

    fixed The End

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ EXAM_SECTIONS = (
     ('Part II. C', timedelta(minutes=1.5)),
     ('Part III.', timedelta(minutes=5)),
     ('Part IV.', timedelta(minutes=3)),
-    ('The End', timedelta(minutes=3)),
+    ('Part V.', timedelta(minutes=3)),
+    ('The End', timedelta()),
 )
 LIGHT = 'gainsboro'
 DARK = 'black'
@@ -29,8 +30,8 @@ class TimerFrame(Frame):
     ui_light = True
 
     # timer logic
-    actual_section = 0
-    section_remaining = EXAM_SECTIONS[actual_section][1]
+    _actual_section = 0
+    section_remaining = EXAM_SECTIONS[_actual_section][1]
     timer_id = None
 
     def __init__(self, master=None, cnf={}, **kw):
@@ -39,7 +40,7 @@ class TimerFrame(Frame):
         self.COPYRIGHT_FONT = Font(master, family='Helvetica', size=28)
         self.LABELS_FONT = Font(master, family='Helvetica', size=50)
         self.SECTION_FONT = Font(master, family='Helvetica', size=76)
-        self.TIME_FONT = Font(master, family='Helvetica', size=96, weight=BOLD)
+        self.TIME_FONT = Font(master, family='Helvetica', size=130, weight=BOLD)
 
         self.setup_ui()
         self._update_current_time()
@@ -53,8 +54,8 @@ class TimerFrame(Frame):
         self.start_btn = Button(self, command=self.start_timer, text='START!', font=self.LABELS_FONT)
         self.start_btn.grid(row=2, column=1, sticky=S)
 
-        self.reset_btn = Button(self, command=self.start_timer,
-        	text='VYNULOVAT!', font=self.COPYRIGHT_FONT
+        self.reset_btn = Button(self, command=self.reset_timer,
+            text='VYNULOVAT!', font=self.COPYRIGHT_FONT
         )
         self.reset_btn.grid(row=2, column=2, sticky=S + E)
 
@@ -64,7 +65,7 @@ class TimerFrame(Frame):
         self.copyright_lbl = Label(self, text='© Josef Kolář, 2016', font=self.COPYRIGHT_FONT)
         self.copyright_lbl.grid(column=2, row=0, sticky=N + E)
 
-        self.section_title_lbl = Label(self, text=EXAM_SECTIONS[0][0], font=self.SECTION_FONT)
+        self.section_title_lbl = Label(self, font=self.SECTION_FONT)
         self.section_title_lbl.grid(column=1, row=0, sticky=N)
 
         self.elapsed_time_lbl = Label(self, text='0:00', font=self.LABELS_FONT)
@@ -74,7 +75,7 @@ class TimerFrame(Frame):
         self.remaining_time_frame.grid(column=1, row=1)
 
         self.remaining_time_lbl = Label(self.remaining_time_frame,
-        	text=format_delta(EXAM_SECTIONS[0][1]), font=self.TIME_FONT
+            text=format_delta(EXAM_SECTIONS[0][1]), font=self.TIME_FONT
         )
         self.remaining_time_lbl.pack()
 
@@ -87,6 +88,8 @@ class TimerFrame(Frame):
             self.elapsed_time_lbl,
             self.reset_btn
         ))
+
+        self.refresh_section()
 
     def _update_current_time(self):
         self.current_time_lbl.configure(text=time.strftime('%H:%M:%S'))
@@ -109,7 +112,7 @@ class TimerFrame(Frame):
         self.section_remaining -= timedelta(seconds=1)
 
         if self.section_remaining.total_seconds() == 0:
-            self._set_next_section()
+            self.actual_section += 1
         elif self.section_remaining.total_seconds() <= 5:
             self._invert_ui()
 
@@ -117,20 +120,37 @@ class TimerFrame(Frame):
         self.elapsed_time_lbl.configure(
             text=format_delta(EXAM_SECTIONS[self.actual_section][1] - self.section_remaining)
         )
-        self.timer_id =self.master.after(1000, self.update_timer)
+        if self.section_remaining.total_seconds() > 0:
+            self.timer_id = self.master.after(100, self.update_timer)
 
-    def _set_next_section(self):
-        self.actual_section += 1
+    def stop_timer(self):
+        if self.timer_id:
+            self.master.after_cancel(self.timer_id)
+        self.start_btn.configure(text='START!', command=self.start_timer)
+
+    def reset_timer(self):
+        if self.timer_id:
+            self.master.after_cancel(self.timer_id)
+        self.actual_section = 0
+        self.start_btn.configure(text='START!', command=self.start_timer)
+
+    @property
+    def actual_section(self):
+        return self._actual_section
+
+    @actual_section.setter
+    def actual_section(self, new):
+        self._actual_section = new
+        self.refresh_section()
+
+    def refresh_section(self):
         section_title, section_length = EXAM_SECTIONS[self.actual_section]
         self.section_title_lbl.configure(text=section_title)
         self.section_remaining = copy(section_length)
-
-    def stop_timer(self):
-    	self.master.after_cancel(self.timer_id)
-    	self.start_btn.configure(text='START!', command=self.start_timer)
-
-    def reset(self):
-    	pass
+        self.remaining_time_lbl.configure(text=format_delta(self.section_remaining))
+        self.elapsed_time_lbl.configure(
+            text=format_delta(EXAM_SECTIONS[self.actual_section][1] - self.section_remaining)
+        )
 
 
 if __name__ == '__main__':
@@ -140,5 +160,6 @@ if __name__ == '__main__':
 
     timer_frame.pack(fill=BOTH)
 
+    master.minsize(900, 350)
     master.attributes('-zoomed', True)
     master.mainloop()
